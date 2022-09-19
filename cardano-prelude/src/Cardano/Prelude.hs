e243fcbb4cd9a53b2e746720a34af6089968c303
John Ky <john.ky@iohk.io>
2022-09-19 23:32:29 +1000

    Add missing microlens instances

--- cardano-prelude/src/Cardano/Prelude.hs
+++ cardano-prelude/src/Cardano/Prelude.hs
@@ -11,5 +11,6 @@ import Cardano.Prelude.Formatting as X
 import Cardano.Prelude.GHC.Heap as X
 import Cardano.Prelude.Json.Canonical as X
 import Cardano.Prelude.Json.Parse as X
+import Cardano.Prelude.Microlens as X ()
 import Cardano.Prelude.Orphans ()
 import Cardano.Prelude.Strict as X
