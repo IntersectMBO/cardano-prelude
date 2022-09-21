01abc011769e2db37d8ee65687d441501a831375
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-09-21 16:52:39 +1200

    Update to latest haskell.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  # This is a template created by `hix init`
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.ghc-next-packages = {
+    url = "github:input-output-hk/ghc-next-packages?ref=repo";
+    flake = false;
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix, ghc-next-packages }:
+    let
+      ci = (builtins.fromTOML (__readFile ./ci.toml)).dimensions;
+    in
+      flake-utils.lib.eachSystem ci.os (system:
+      let
+        compilers = builtins.filter
+          (x: !__elem "${system}.${x}" ci.disable)
+          (ci.compiler);
+        overlays = [ haskellNix.overlay
+          (final: prev: {
+            hixProject =
+              final.haskell-nix.hix.project {
+                src = ./.;
+                evalSystem = "x86_64-linux";
+                compiler-nix-name = __head compilers;
+                inputMap."https://input-output-hk.github.io/ghc-next-packages" = ghc-next-packages;
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake {
+          variants = pkgs.lib.genAttrs (__tail compilers)
+            (x: { compiler-nix-name = pkgs.lib.mkForce x; });
+        };
+      in flake // {
+        legacyPackages = pkgs;
+      });
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
+}
---
+++ nix/hix.nix
@@ -0,0 +1,37 @@
+{config, pkgs, ...}: {
+  name = "cardano-prelude";
+
+  modules = [{
+    packages.cardano-prelude.configureFlags = [ "--ghc-option=-Werror" ];
+  }];
+
+  shell = {
+    tools = {
+      cabal = "latest";
+      ghcid = "latest";
+      hlint = "latest";
+      weeder = { ghc8107 = "2.2.0"; }.${config.compiler-nix-name} or "latest";
+      # haskell-language-server = "latest";
+    };
+
+    buildInputs = [
+      pkgs.buildPackages.gitAndTools.git
+      pkgs.buildPackages.pkgconfig
+      pkgs.buildPackages.sqlite-interactive
+    ];
+
+    # Skip cross compilers for the shell
+    crossPlatforms = p: [];
+
+    exactDeps = false;
+  };
+
+  # Cross compilation support:
+  crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
+     p.mingwW64
+  ] ++ pkgs.lib.optionals (config.compiler-nix-name == "ghc8107") [
+     p.ghcjs
+  ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+     p.musl64
+  ]);
+}
