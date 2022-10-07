306680e8d847485884489a08224ba083e458a093
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-10-07 20:59:17 +1300

    Use nix flake, tullia and cicero

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  # This is a template created by `hix init`
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:input-output-hk/flake-compat/fixes";
+      flake = false;
+    };
+    tullia = {
+       url = "github:input-output-hk/tullia";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+     ghc-next-packages = {
+      url = "github:input-output-hk/ghc-next-packages?ref=repo";
+      flake = false;
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix, flake-compat, tullia, ghc-next-packages }:
+    let
+      ci = (builtins.fromTOML (__readFile ./ci.toml)).dimensions;
+    in
+      flake-utils.lib.eachSystem ci.os (system:
+      let
+        compilers = builtins.filter
+          (x: !__elem "${system}.${x}" ci.disable)
+          (ci.compiler);
+        overlays = [ haskellNix.overlay
+          (final: prev: {
+            hixProject =
+              final.haskell-nix.hix.project {
+                src = ./.;
+                evalSystem = "x86_64-darwin";
+                compiler-nix-name = __head compilers;
+                inputMap."https://input-output-hk.github.io/ghc-next-packages" = ghc-next-packages;
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake {
+          variants = pkgs.lib.genAttrs (__tail compilers)
+            (x: { compiler-nix-name = pkgs.lib.mkForce x; });
+        };
+      in flake // {
+        legacyPackages = pkgs;
+      } // tullia.fromSimple system (import ./nix/tullia.nix self system));
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
+}
---
+++ nix/tullia.nix
@@ -0,0 +1,78 @@
+self: system:
+
+let
+  ciInputName = "GitHub event";
+in rec {
+  tasks = let
+    inherit (self.inputs.tullia) flakeOutputTasks taskSequence;
+
+    common = {
+      config,
+      ...
+    }: {
+      preset = {
+        # needed on top-level task to set runtime options
+        nix.enable = true;
+
+        github-ci = {
+          enable = config.actionRun.facts != {};
+          repo = "input-output-hk/cardano-node";
+          sha = config.preset.github-ci.lib.getRevision ciInputName null;
+        };
+      };
+    };
+
+    mkHydraJobTask = flakeOutputTask: {...}: {
+      imports = [common flakeOutputTask];
+
+      # some hydra jobs run NixOS tests
+      env.NIX_CONFIG = ''
+        extra-system-features = kvm
+      '';
+
+      memory = 1024 * 8;
+      nomad.resources.cpu = 10000;
+    };
+    mkHydraJobTasks = __mapAttrs (_: mkHydraJobTask);
+
+    hydraJobTasks   = mkHydraJobTasks (flakeOutputTasks ["hydraJobs"] self);
+    # TODO this should this be using `hydraJobsPr` instead of `hydraJobs`?
+    # Where does that come from?
+    hydraJobPrTasks = mkHydraJobTasks (flakeOutputTasks ["hydraJobs"] self);
+
+    ciPushTasks = taskSequence "ci/push/" hydraJobTasks   (__attrNames hydraJobTasks);
+    ciPrTasks   = taskSequence "ci/pr/"   hydraJobPrTasks (__attrNames hydraJobPrTasks);
+  in
+    ciPushTasks // ciPrTasks // {
+      "ci/push" = {lib, ...}: {
+        imports = [common];
+        after = [(lib.last (__attrNames ciPushTasks))];
+      };
+
+      "ci/pr" = {lib, ...}: {
+        imports = [common];
+        after = [(lib.last (__attrNames ciPrTasks))];
+      };
+    };
+
+  actions = {
+    "cardano-node/ci/push" = {
+      task = "ci/push";
+      io = ''
+        #lib.io.github_push
+        #input: "${ciInputName}"
+        #repo: "input-output-hk/cardano-node"
+      '';
+    };
+
+    "cardano-node/ci/pr" = {
+      task = "ci/pr";
+      io = ''
+        #lib.io.github_pr
+        #input: "${ciInputName}"
+        #repo: "input-output-hk/cardano-node"
+        #target_default: false
+      '';
+    };
+  };
+}
